60a3fad867e4b2bfcf2598acc3c7ac1f5d0b0961
anthonyvu <Anthonyvu982@hotmail.com>
2021-02-05 10:56:22 +0100

    Kode for tutorial til å vise brettet

--- src/main/java/inf112/skeleton/app/HelloWorld.java
+++ src/main/java/inf112/skeleton/app/HelloWorld.java
@@ -4,18 +4,63 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
+import com.badlogic.gdx.math.Vector2;
 
 public class HelloWorld implements ApplicationListener {
     private SpriteBatch batch;
     private BitmapFont font;
+
+    private TiledMap map;
+    private TiledMapTileLayer board,player,hole, flag;
+
+    private OrthogonalTiledMapRenderer render;
+    private OrthographicCamera camera;
+
+    private TiledMapTileLayer.Cell playerCell, wonPlayerCell, deadPlayerCell;
+
+    Vector2 playerPosition;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         font = new BitmapFont();
         font.setColor(Color.RED);
+
+        TmxMapLoader loader = new TmxMapLoader();
+        map = loader.load("tutorial.tmx");
+
+        //Layers initialize
+        board = (TiledMapTileLayer) map.getLayers().get("Board");
+        player = (TiledMapTileLayer) map.getLayers().get("Player");
+        hole = (TiledMapTileLayer) map.getLayers().get("Hole");
+        flag = (TiledMapTileLayer) map.getLayers().get("Flag");
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false,5,5);
+
+
+        camera.update();
+
+        render = new OrthogonalTiledMapRenderer(map, 1/300f);
+
+        render.setView(camera);
+
+        Texture texture = new Texture("player.png");
+
+        TextureRegion[][] tr = TextureRegion.split(texture,300,300);
+
+        playerCell = new TiledMapTileLayer.Cell();
+        playerCell.setTile(new StaticTiledMapTile(tr[0][0]));
     }
 
     @Override
