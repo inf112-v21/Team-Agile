50215572ff2650d624703accd917f52c1d650740
Marius <marius.skimmeland@student.uib.no>
2021-02-24 13:17:03 +0100

    lagt til Deck.java & Programcard.java til branch playingcards

---
+++ src/main/java/inf112/skeleton/app/Cards/Deck.java
@@ -0,0 +1,62 @@
+package inf112.skeleton.app.Cards;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck {
+
+    public Programcard[] cards;
+    private int cardsInDeck;
+
+
+    public Deck() {
+
+        cards = new Programcard[84];
+
+    }
+
+    public void reset() {
+
+        cardsInDeck = 0;
+        // Programcard.Direction[] directions = new Programcard.Direction.values();
+
+        //BackUP kort
+        for (int x = 430; x <= 480; x += 10) {
+
+            //    cards[cardsInDecks++] = new Programcard(priotity(x), )
+
+        }
+
+    }
+
+
+    //public void shuffleDeck() {
+    //    Collections.shuffle(cards);
+}
+
+//public void createDeck() {
+
+
+//for (int i = 80; i <= 420 ; i += 20) {
+//   Programcard card = new Programcard();
+//}
+
+//    }
+
+//}
+
+
+
+    /*
+har en bevegelse (Flytt frem 1, Flytt frem 2, Flytt frem 3, Flytt bakover 1, Roter 90 grader H, Roter 90 grader V, Roter 180 grader)
+har en prioritet (alle bevegelseskort må ha unik prioritet innenfor sin klasse, og mellom klassene (bevegelsestypene) skal intervallet ikke være overlappende)
+backup: 6 kort (430 - 480, intervall 10)
+u-turn: 6 kort (10 - 60, intervall 10)
+rotate right: 18 kort (80-420, intervall 20)
+rotate left: 18 kort (70-410, intervall 20)
+move 1: 18 kort (490 - 650, intervall 10)
+move 2: 12 kort (670 - 780, intervall 10)
+move 3: 6 kort (790 - 840, intervall 10)
+
+ */
+
---
+++ src/main/java/inf112/skeleton/app/Cards/Programcard.java
@@ -0,0 +1,68 @@
+package inf112.skeleton.app.Cards;
+
+import org.lwjgl.system.windows.MONITORINFOEX;
+
+public class Programcard {
+
+
+    public enum MoveType {
+        BACKUP,
+        UTURN,
+        ROTATE_RIGHT,
+        ROTATE_LEFT,
+        MOVEONE,
+        MOVETWO,
+        MOVETHREE;
+    }
+
+
+    public enum Direction {
+        SOUTH,
+        WEST,
+        NORTH,
+        EAST;
+
+        private static final Direction[] dir = Direction.values();
+
+        public static Direction getDir(int i) {
+            return Direction.dir[i];
+        }
+    }
+
+
+    /*
+    libgdx metode rotate på selve Sprite (bilde objektet) brukes med angitt grader. (0-360) , 0 samme, 90
+     */
+
+    MoveType type;
+    int priority;
+    Direction direction;
+
+    public Programcard(MoveType type, int priority, Direction direction) {
+        this.type = type;
+        this.priority = priority;
+        this.direction = direction;
+    }
+
+    public MoveType getSteps() {
+        return type;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+}
+/*
+Lage 1 klasse for kortene.
+kort som endre direction (priority, direction)
+kort som bevege antall steps i nåverende direction (steps, priority, direction (kan droppes) )
+steps (0, 0, VEST)
+steps (0, 1, NONE)
+arraylist<Kortklasse>
+2 arrays
+ */
