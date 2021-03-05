027b5ff4ea9c72322d5644cd42db97f879fca605
Marius <marius.skimmeland@student.uib.no>
2021-03-05 12:43:19 +0100

    noen småendringer. fjernet noen ubrukte imports

--- src/main/java/inf112/skeleton/app/Flag.java
+++ src/main/java/inf112/skeleton/app/Flag.java
@@ -3,20 +3,11 @@ package inf112.skeleton.app;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.math.Vector2;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 public class Flag {
-
-
     TiledMapTileLayer flagLayer;
     Integer flagID;
 
     Vector2 position;
-    TiledMapTileLayer Layer;
-
-
-
 
 
     public Flag(TiledMapTileLayer flagLayer, Integer flagID, Vector2 flagpos, float xPos, float yPos) {
@@ -27,17 +18,12 @@ public class Flag {
 
         flagLayer.setCell((int) position.x, (int) position.y, null );
         position.set(position.x + xPos, position.y + yPos);
-
-
-
     }
 
     public Flag(Vector2 flagpos, Integer flagID) {
         this.position = flagpos;
         this.flagID = flagID;
     }
-
-
 
 }
 
