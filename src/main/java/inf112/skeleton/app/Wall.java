6945adecd5b1558c6fa649f4135ac095ab08b9f4
LeifPetter98 <Leifpetters@gmail.com>
2021-03-15 13:49:40 +0100

    såvidt startet på wall-klasse

---
+++ src/main/java/inf112/skeleton/app/Wall.java
@@ -0,0 +1,11 @@
+package inf112.skeleton.app;
+
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.math.Vector2;
+
+public class Wall {
+    //vegger skal stoppe spille og laser.
+    public Wall(TiledMapTileLayer wallLayer, Vector2 wallPos){
+
+    }
+}
