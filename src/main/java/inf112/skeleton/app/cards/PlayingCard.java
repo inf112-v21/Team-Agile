6c0676ad2ebdc58c848350a20fe1c27b0eb7f886
Marius <marius.skimmeland@student.uib.no>
2021-03-18 21:12:44 +0100

    slettet noen unødgie linjer

--- src/main/java/inf112/skeleton/app/cards/PlayingCard.java
+++ src/main/java/inf112/skeleton/app/cards/PlayingCard.java
@@ -36,15 +36,3 @@ public class PlayingCard extends Sprite {
 
 
 }
-
-
-
-/*
-Lage 1 klasse for kortene.
-kort som endre direction (priority, direction)
-kort som bevege antall steps i nåverende direction (steps, priority, direction (kan droppes) )
-steps (0, 0, VEST)
-steps (0, 1, NONE)
-arraylist<Kortklasse>
-2 arrays
- */
