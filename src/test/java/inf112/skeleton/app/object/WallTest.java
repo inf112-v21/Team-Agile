ba684906ac324a0e8aeaf1ce127fbed758713cae
LeifPetter98 <Leifpetters@gmail.com>
2021-03-25 17:25:18 +0100

    startet på tester til vegg klassen. sjekket for sør retning. mangler sjekk av neste tile også

---
+++ src/test/java/inf112/skeleton/app/object/WallTest.java
@@ -0,0 +1,74 @@
+package inf112.skeleton.app.object;
+
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Vector2;
+import inf112.skeleton.app.RoboRally;
+import inf112.skeleton.app.map.Wall;
+import inf112.skeleton.app.object.Robot;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class WallTest {
+
+
+    Wall wall;
+    Vector2 wallPos;
+    int cellID;
+    Robot robot;
+    RoboRally game = new RoboRally("tutorial.tmx");
+
+
+    @Before
+    public void setUp() throws Exception {
+        Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
+        cfg.setTitle("CLOSE THIS WINDOW TO START THE TESTS");
+        cfg.setWindowedMode(500, 100);
+
+
+
+        new Lwjgl3Application(new Game() {
+            @Override
+            public void create() {
+                Gdx.app.exit();
+            }
+        }, cfg);
+    }
+
+    @Test
+    public void testIfWallIsInFrontOfPlayerSouth(){
+         robot = new Robot(3,3, Color.BLACK,1,game);
+         robot.setRotation(0);
+         wall = new Wall(new Vector2(3,3), 29 );
+         game.allWalls.add(wall);
+         robot.move(1);
+
+
+         assertEquals(3, (int) robot.getX());
+         assertEquals(3, (int) robot.getY());
+    }
+
+    @Test
+    public void testIfWallIsInFrontOfPlayerNorth(){
+        robot = new Robot(3,3, Color.BLACK,1,game);
+        robot.setRotation(0);
+        wall = new Wall(new Vector2(3,3), 29 );
+        game.allWalls.add(wall);
+        robot.move(1);
+
+
+        assertEquals(3, (int) robot.getX());
+        assertEquals(3, (int) robot.getY());
+    }
+
+
+
+}
