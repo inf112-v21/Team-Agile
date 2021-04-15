fb3b8c0d15ff1604da262a501e91aa4e974161cc
tranjp <johnny.p.t@hotmail.com>
2021-04-15 20:55:09 +0200

    Booster number-identifier

---
+++ src/main/java/inf112/skeleton/app/map/Booster.java
@@ -0,0 +1,30 @@
+package inf112.skeleton.app.map;
+
+public class Booster {
+
+
+    //Single straight booster
+    public static int UP = 49;
+    public static int DOWN = 50;
+    public static int LEFT = 51;
+    public static int RIGHT = 52;
+
+    //Single turn booster
+    //mot klokken
+    public static int LEFTDOWN = 33;
+    //med klokken
+    public static int RIGHTDOWN = 36;
+
+    //Double straight booster
+    public static int UPP = 13;
+    public static int DOWNN = 21;
+    public static int LEFTT = 22;
+
+    //Double turn booster
+    //med klokken
+    public static int DOWNLEFT = 28;
+    public static int LEFTUPUP = 77;
+
+
+
+}
